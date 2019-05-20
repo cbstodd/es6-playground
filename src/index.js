83c3e31ddab8b04dad1ed69e63f1a288d4ea4b14
cbstodd <cbstodd@gmail.com>
2019-05-19 22:34:04 -0400

    Refactored todo app to use a form submit versus a click event

--- src/index.js
+++ src/index.js
@@ -29,16 +29,21 @@ const filters = {
     searchText: ''
 };
 
-let newTodoBody = '';
 
 // GETS THE AMOUNT OF INCOMPLETE TODOS:
-const incompleteTodos = todoList.filter((todo) => {
-    return !todo.completed;
-});
-// PRINTS THE NUMBER OF INCOMPLETE TODOS ON THE PAGE:
-const numOfIncompTodos = document.createElement('span');
-numOfIncompTodos.textContent = incompleteTodos.length;
-document.querySelector('.incomp-todos').appendChild(numOfIncompTodos);
+const renderIncompleteTodos = () => {
+    const incompleteTodos = todoList.filter((todo) => {
+        return !todo.completed;
+    });
+
+    document.getElementById('incompleteTodos').innerHTML = '';
+    // PRINTS THE NUMBER OF INCOMPLETE TODOS ON THE PAGE:
+    const numOfIncompTodos = document.createElement('span');
+
+    numOfIncompTodos.innerHTML = incompleteTodos.length;
+    document.getElementById('incompleteTodos').appendChild(numOfIncompTodos);
+};
+
 
 // Renders todos to page:
 const renderTodos = (todos, filters) => {
@@ -58,35 +63,8 @@ const renderTodos = (todos, filters) => {
         }
     });
 };
-
+renderIncompleteTodos();
 renderTodos(todoList, filters);
-
-// Click event that changes text on button when clicked:
-document.getElementById('addTodoBtn').addEventListener('click', (e) => {
-    e.target.textContent = `${newTodoBody} was added!`;
-    setTimeout(() => {
-        e.target.innerHTML = `<i class="fa fa-plus"></i> Add Todo`;
-    }, 2000);
-
-
-    addTodo(newTodoBody);
-});
-
-// Adds click event to remove all todos in unordered list:
-document.getElementById('clearTodos').addEventListener('click', (e) => {
-    document.querySelectorAll('#todos').forEach((todo) => {
-        todo.remove();
-    });
-});
-
-document.getElementById('searchTodoInput').addEventListener('input', (e) => {
-    filters.searchText = e.target.value.toLowerCase();
-    renderTodos(todoList, filters);
-});
-
-document.getElementById('addTodoInput').addEventListener('input', (e) => {
-    newTodoBody = e.target.value;
-});
 
 const addTodo = (todoBody) => {
     const randNum = Math.floor((Math.random() * 1999999) + 1);
@@ -97,5 +75,33 @@ const addTodo = (todoBody) => {
     };
     todoList.push(newTodo);
     console.table(todoList);
+    renderIncompleteTodos();
     renderTodos(todoList, filters);
 };
+
+const alertBtnMsg = (_newTodoBody) => {
+    // Updates text on button (alert):
+    const newTodoBtn = document.getElementById('addTodoBtn');
+    newTodoBtn.textContent = `"${_newTodoBody}" was added!!!`;
+    setTimeout(() => {
+        newTodoBtn.innerHTML = `<i class="fa fa-plus"></i> Add Todo`;
+    }, 2000);
+};
+
+
+// ####################### DOM EVENT HANDLERS ######################
+document.getElementById('searchTodoInput').addEventListener('input', (e) => {
+    filters.searchText = e.target.value.toLowerCase();
+    renderTodos(todoList, filters);
+});
+
+// Add New Todo Form:
+document.getElementById('newTodoForm').addEventListener('submit', (e) => {
+    // Prevents Browser from reloading:
+    e.preventDefault();
+    // 1. Adds todo to list; 2. Alerts Success; 3. Clears form input:
+    const newTodoBody = e.target.elements.newTodo.value;
+    addTodo(newTodoBody);
+    alertBtnMsg(newTodoBody);
+    e.target.elements.newTodo.value = '';
+});
