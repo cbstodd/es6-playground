003bc6f9ac69d6cf4a1c992f408caf145a240394
cbstodd <cbstodd@gmail.com>
2019-05-23 16:52:24 -0400

    Commmit before refactor

---
+++ src/todo-app/data.js
@@ -0,0 +1,59 @@
+// ####################### DATA ##########################
+const addTodosToLocalStorage = (todos) => {
+    if (todos) {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    } else {
+        return null;
+    }
+};
+
+// FILTERS:
+const getFilters = () => {
+    return {
+        searchText: '',
+        hideCompleted: false,
+        sortType: ''
+    };
+};
+
+// CHECK FOR EXISTING TODOS:
+const getSavedTodos = () => {
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON);
+    } else {
+        return [];
+    }
+};
+
+
+
+
+// export function data() {
+//     return [{
+//             id: `${Math.floor((Math.random() * 1999999) + 1)}`,
+//             body: 'Make Coffee',
+//             completed: true
+//         },
+//         {
+//             id: `${Math.floor((Math.random() * 1999999) + 1)}`,
+//             body: 'Uncover porch cushions',
+//             completed: true
+//         },
+//         {
+//             id: `${Math.floor((Math.random() * 1999999) + 1)}`,
+//             body: 'Go for a walk at Quiet Waters',
+//             completed: false
+//         },
+//         {
+//             id: `${Math.floor((Math.random() * 1999999) + 1)}`,
+//             body: 'Study Web Flutter',
+//             completed: false
+//         },
+//         {
+//             id: `${Math.floor((Math.random() * 1999999) + 1)}`,
+//             body: 'Fill out ngConf expense report',
+//             completed: false
+//         },
+//     ];
+// }
