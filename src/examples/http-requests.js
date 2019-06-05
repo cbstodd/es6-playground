88c390c4f34c3d956c6a40e534e698cecfe5a498
cbstodd <cbstodd@gmail.com>
2019-06-04 22:47:21 -0400

    Added https GET example via constructor fun vs eventEmitter

--- src/examples/http-requests.js
+++ src/examples/http-requests.js
@@ -1,29 +1,30 @@
-const request = new XMLHttpRequest();
-const loc = 'Annapolis';
-const api = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=imperial&appid=437b7c9d5259eb873a2df29076a0f4df`;
+// const getWeather = (callback) => {
+//     request.addEventListener('readystatechange', (evt) => {
+//         const et = evt.target;
 
-request.open('GET', api);
-request.send();
+//         if (et.readyState === 4 && et.status === 200) {
+//             const data = JSON.parse(et.responseText);
+//             callback(undefined, data);
+//         } else if (et.status !== 200) {
+//             callback(et, undefined);
+//         }
+//     });
+// };
 
-const getWeather = (callback) => {
-    request.addEventListener('readystatechange', (evt) => {
-        const et = evt.target;
+const getWeather = () => {
+    const request = new XMLHttpRequest();
+    const loc = 'Annapolis';
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=imperial&appid=437b7c9d5259eb873a2df29076a0f4df`;
 
-        if (et.readyState === 4 && et.status === 200) {
-            const data = JSON.parse(et.responseText);
-            callback(undefined, data);
-        } else if (et.status !== 200) {
-            callback(et, undefined);
-        }
-    });
+    request.open('GET', api, false);
+    request.send();
+    const data = JSON.parse(request.responseText);
+
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(`The weather in ${data.name} is: ${data.weather[0].description} with the temperature of ${Math.floor(data.main.temp)}(F).`);
+    } else if (request.status !== 200) {
+        throw Error(`The API URL was not found. a "${data.status}" status was received.`);
+    }
 };
 
-getWeather((error, data) => {
-    if (error) {
-        throw Error(`The API URL was not found. a "${error.status}" status was received.`);
-    } else {
-        console.log(`The weather in ${data.name} is: ${data.weather[0].description} with the temperature of ${Math.floor(data.main.temp)}(F).`);
-    }
-});
-
-// getWeather();
+getWeather();
